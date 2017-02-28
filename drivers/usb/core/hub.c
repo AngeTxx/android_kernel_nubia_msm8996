bf138543d9f735d19178c3e255fc488b51e20e44
XiNGRZ <chenxingyu92@gmail.com>
2017-03-01 01:03:58 +0800

    Import NX531J_M_kernel: drivers/usb
    Change-Id: Id7d3da12173355fc2708c61f4542ba68d7641267

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4174,6 +4174,7 @@ static int hub_set_address(struct usb_device *udev, int devnum)
  * device says it supports the new USB 2.0 Link PM errata by setting the BESL
  * support bit in the BOS descriptor.
  */
+#if 0 //modify to solve PR00331633/00331634/00331637/00331638 by yesichao
 static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 {
 	struct usb_hub *hub = usb_hub_to_struct_hub(udev->parent);
@@ -4191,6 +4192,7 @@ static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 		usb_set_usb2_hardware_lpm(udev, 1);
 	}
 }
+#endif
 
 static int hub_enable_device(struct usb_device *udev)
 {
@@ -4510,7 +4512,9 @@ hub_port_init (struct usb_hub *hub, struct usb_device *udev, int port1,
 	/* notify HCD that we have a device connected and addressed */
 	if (hcd->driver->update_device)
 		hcd->driver->update_device(hcd, udev);
+#if 0 //modify to solve PR00331633/331634/331637/331638 by yesichao
 	hub_set_initial_usb2_lpm_policy(udev);
+#endif
 fail:
 	if (retval) {
 		hub_port_disable(hub, port1, 0);
