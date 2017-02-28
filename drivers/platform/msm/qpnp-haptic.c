a8ac1f2ae1643e6ba74556db7eb3225e29ce4c2b
XiNGRZ <chenxingyu92@gmail.com>
2017-03-01 01:07:11 +0800

    Import NX531J_M_kernel: drivers/platform/msm
    Change-Id: Ieb86c56b2895d535c681eb7624e79eabadd6f162

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -151,6 +151,13 @@
 #define POLL_TIME_AUTO_RES_ERR_NS	(5 * NSEC_PER_MSEC)
 
 #define LRA_POS_FREQ_COUNT		6
+
+#ifdef HAPTIC_DEBUG
+	#define hap_debug(format , ...)   printk(format, ##__VA_ARGS__)
+#else
+	#define hap_debug(format , ...)
+#endif
+
 int lra_play_rate_code[LRA_POS_FREQ_COUNT];
 
 /* haptic debug register set */
@@ -330,6 +337,9 @@ struct qpnp_hap {
 	bool sup_brake_pat;
 	bool correct_lra_drive_freq;
 	bool misc_trim_error_rc19p2_clk_reg_present;
+#ifdef CONFIG_FEATURE_ZTEMT_HAPTIC_VIBRATOR
+	u32 ztemt_vibrator_ms;
+#endif
 };
 
 static struct qpnp_hap *ghap;
@@ -1564,6 +1574,10 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 		}
 		hap->state = 0;
 	} else {
+#ifdef CONFIG_FEATURE_ZTEMT_HAPTIC_VIBRATOR
+		hap_debug("--------app time = %d\n", value);
+		value = value +hap->ztemt_vibrator_ms;
+#endif
 		value = (value > hap->timeout_ms ?
 				 hap->timeout_ms : value);
 		hap->state = 1;
@@ -1982,6 +1996,18 @@ static int qpnp_hap_parse_dt(struct qpnp_hap *hap)
 		dev_err(&spmi->dev, "Unable to read timeout\n");
 		return rc;
 	}
+
+#ifdef CONFIG_FEATURE_ZTEMT_HAPTIC_VIBRATOR
+	hap->ztemt_vibrator_ms = 0;
+	rc = of_property_read_u32(spmi->dev.of_node,
+		"qcom,ztemt_vibrator_ms",&temp);
+	if (!rc) {
+		hap->ztemt_vibrator_ms = temp;
+	} else if (rc != -EINVAL) {
+		dev_err(&spmi->dev, "Unable to read ztemt_vibrator_ms\n");
+		return rc;
+	}
+#endif
 
 	hap->act_type = QPNP_HAP_LRA;
 	rc = of_property_read_string(spmi->dev.of_node,
